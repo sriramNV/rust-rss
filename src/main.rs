b7df2298f2ddfaed401af222b048f048f1f69acf
Sriram <sriramnvilvanathan@gmail.com>
2025-07-19 11:43:51 +0530

    added basic font

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
-use eframe::egui::CentralPanel;
+use eframe::egui::{CentralPanel, FontFamily, FontId, TextStyle, Context};
 
 #[derive(Default)]
 struct App{}
 
 impl eframe::App for App{
-    fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
+    fn update(
+            &mut self, 
+            ctx: &eframe::egui::Context, 
+            _frame: &mut eframe::Frame
+    ) {
+        set_styles(ctx);
         CentralPanel::default().show(ctx, |ui|{
             ui.heading("Hello World")
         });
@@ -19,4 +24,11 @@ fn main()  -> Result<(), eframe::Error> {
         ..Default::default()
     };
     eframe::run_native("Rustdev", options, Box::new(|_cc|Ok(Box::<App>::default())))
+}
+
+fn set_styles(ctx: &Context) {
+    let mut style = (*ctx.style()).clone();
+    style.text_styles = [(TextStyle::Heading, FontId::new(30.0, FontFamily::Monospace))].into();
+
+    ctx.set_style(style)
 }
