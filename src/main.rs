ad93e567f37ac0a2070d466c202a30a2e634c209
Sriram <sriramnvilvanathan@gmail.com>
2025-07-19 12:55:23 +0530

    minor fixes

--- src/main.rs
+++ src/main.rs
@@ -129,7 +129,7 @@ fn main()  -> Result<(), eframe::Error> {
         viewport: eframe::egui::ViewportBuilder::default().with_resizable(true).with_inner_size([320.0,240.0]),
         ..Default::default()
     };
-    eframe::run_native("Rustdev", options, Box::new(|_cc|Ok(Box::<App>::default())))
+    eframe::run_native("FetchRSS :)", options, Box::new(|_cc|Ok(Box::<App>::default())))
 }
 
 fn set_styles(ctx: &Context) {
